a185790806eb5769ac67961cb7821bdb8901ea78
Quentayro <quentayro+github@gmail.com>
2023-05-02 14:10:09 +0200

    injection dependance

--- src/main/java/com/example/demo/api/ToDoListAPI.java
+++ src/main/java/com/example/demo/api/ToDoListAPI.java
@@ -1,6 +1,5 @@
 package com.example.demo.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +12,11 @@ import com.example.demo.service.ToDoListService;
 
 @RestController
 public class ToDoListAPI {
-    @Autowired
     private ToDoListService toDoListService;
+
+    public ToDoListAPI(ToDoListService toDoListService) {
+        this.toDoListService = toDoListService;
+    }
 
     @PostMapping("/add")
     public ResponseEntity<String> addItem(@RequestBody ToDoListItem toDoListItem) {
